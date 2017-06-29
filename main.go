015ebfec293b9c97910f6b4d81da9bc5f527b32c
Nico Schieder <nico.schieder@hetzner-cloud.de>
2017-06-29 23:27:48 +0200

    init

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/golang/glog"
+
+	"gitlab.thetechnick.ninja/thetechnick/nginx-ingress/pkg/controller"
+	"gitlab.thetechnick.ninja/thetechnick/nginx-ingress/pkg/nginx"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+var (
+	// Set during build
+	version string
+
+	healthStatus = flag.Bool("health-status", false,
+		`If present, the default server listening on port 80 with the health check
+		location "/nginx-health" gets added to the main nginx configuration.`)
+
+	proxyURL = flag.String("proxy", "",
+		`If specified, the controller assumes a kubctl proxy server is running on the
+		given url and creates a proxy client. Regenerated NGINX configuration files
+    are not written to the disk, instead they are printed to stdout. Also NGINX
+    is not getting invoked. This flag is for testing.`)
+
+	watchNamespace = flag.String("watch-namespace", api.NamespaceAll,
+		`Namespace to watch for Ingress/Services/Endpoints. By default the controller
+		watches acrosss all namespaces`)
+
+	nginxConfigMaps = flag.String("nginx-configmaps", "",
+		`Specifies a configmaps resource that can be used to customize NGINX
+		configuration. The value must follow the following format: <namespace>/<name>`)
+)
+
+func main() {
+	flag.Parse()
+	flag.Lookup("logtostderr").Value.Set("true")
+
+	glog.Infof("Starting NGINX Ingress controller Version %v\n", version)
+
+	var err error
+	var config *rest.Config
+	if *proxyURL != "" {
+		config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			&clientcmd.ClientConfigLoadingRules{},
+			&clientcmd.ConfigOverrides{
+				ClusterInfo: clientcmdapi.Cluster{
+					Server: *proxyURL,
+				},
+			}).ClientConfig()
+		if err != nil {
+			glog.Fatalf("error creating client configuration: %v", err)
+		}
+	} else {
+		if config, err = rest.InClusterConfig(); err != nil {
+			glog.Fatalf("error creating client configuration: %v", err)
+		}
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatalf("Failed to create client: %v.", err)
+	}
+
+	ngxc, _ := nginx.NewNginxController("/etc/nginx/", *proxyURL != "", *healthStatus)
+	ngxc.Start()
+	nginxConfig := nginx.NewDefaultConfig()
+	cnf := nginx.NewConfigurator(ngxc, nginxConfig)
+	lbc, _ := controller.NewLoadBalancerController(kubeClient, 30*time.Second, *watchNamespace, cnf, *nginxConfigMaps)
+	lbc.Run()
+}
